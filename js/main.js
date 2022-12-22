4467da467240c577b850d49e7e53dd7df9f94367
kaliraj <kalisdvk96@gmail.com>
2022-12-22 10:06:58 +0530

    create object from user

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//------->key also from user-----
+// let cricket={};
+// let n=parseInt(prompt("Enter the length"));
+// let keys,values;
+// for(i=0;i<n;i++){
+//     keys=prompt("Enter the key");
+//     values=prompt("Enter the value");
+//     cricket[keys]=values;
+// }
+// console.log(cricket);
+
+let cricket={
+    opener:"",
+    middle:"",
+    allround:"",
+    finisher:"",
+    spin:"",
+    bowler:""
+}
+console.log(cricket);
+cricket.opener=prompt("enter opener");
+cricket.middle=prompt("enter middle");
+cricket.allround=prompt("enter allround");
+cricket.finisher=prompt("enter finisher");
+cricket.spin=prompt("enter spin");
+cricket.bowler=prompt("enter bowler");
+console.log(cricket);
